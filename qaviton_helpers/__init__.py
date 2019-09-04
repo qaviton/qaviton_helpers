565c386b9f19b924db258ec080b55329da13d512
yehonadav <yonadav.barilan@gmail.com>
2019-09-04 12:18:09 +0300

    init

--- qaviton_helpers/__init__.py
+++ qaviton_helpers/__init__.py
@@ -11,6 +11,7 @@ from qaviton_helpers.converters import string_to_ascii, ascii_to_string
 from qaviton_helpers.diff import list_diff
 from qaviton_helpers.inner_info import funcname, classname
 from qaviton_helpers.object_helpers import DynamicClass, lists_to_object, dict_to_object, object_to_dict, object_to_list
+from qaviton_helpers.relative_path import path
 from qaviton_helpers.silly_helpers import get_python_interpreter, get_python_version, get_timestamp, pop_by_name, swap
 from qaviton_helpers.timers import DynamicWait
 from qaviton_helpers.try_functions import try_to, try_or_none
