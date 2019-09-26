c5fad45081ec4980d68502d92200242bc7818b16
yehonadav <yonadav.barilan@gmail.com>
2019-09-26 18:14:59 +0300

    added comments to converters

--- qaviton_helpers/converters.py
+++ qaviton_helpers/converters.py
@@ -1,15 +1,43 @@
 from urllib.parse import quote
 
 
-def urlencode(string):
-    return quote(string, safe='')
+def urlencode(string, safe='', encoding=None, errors=None):
+    """quote('abc def') -> 'abc%20def'
+
+    Each part of a URL, e.g. the path info, the query, etc., has a
+    different set of reserved characters that must be quoted.
+
+    RFC 2396 Uniform Resource Identifiers (URI): Generic Syntax lists
+    the following reserved characters.
+
+    reserved    = ";" | "/" | "?" | ":" | "@" | "&" | "=" | "+" |
+                  "$" | ","
+
+    Each of these characters is reserved in some component of a URL,
+    but not necessarily in all of them.
+
+    By default, the quote function is intended for quoting the path
+    section of a URL.  Thus, it will not encode '/'.  This character
+    is reserved, but in typical usage the quote function is being
+    called on a path where the existing slash characters are used as
+    reserved characters.
+
+    string and safe may be either str or bytes objects. encoding and errors
+    must not be specified if string is a bytes object.
+
+    The optional encoding and errors parameters specify how to deal with
+    non-ASCII characters, as accepted by the str.encode method.
+    By default, encoding='utf-8' (characters are encoded with UTF-8), and
+    errors='strict' (unsupported characters raise a UnicodeEncodeError).
+    """
+    return quote(string, safe=safe, encoding=encoding, errors=errors)
 
 
 def string_to_ascii(string: str):
-    """return string of ascii characters with _ as seperator"""
+    """return string of ascii characters with _ as separator"""
     return '_'.join(str(ord(c)) for c in string)
 
 
 def ascii_to_string(string: str):
-    """return ascii characters with _ as seperator to string"""
+    """return ascii characters with _ as separator to string"""
     return ''.join(chr(int(i)) for i in string.split('_'))
