70ef4db4ce9a4922011c453dbceb41aba8be7a6e
yehonadav <yonadav.barilan@gmail.com>
2019-10-03 16:20:28 +0300

    build candidate 2019.10.3.13.20.28.843213

---
+++ qaviton_helpers/timeunit.py
@@ -0,0 +1,10 @@
+nanosecond = 1/1000000000
+microsecond = 1/1000000
+millisecond = 1/1000
+second = 1
+minute = 60 * second
+hour = 60 * minute
+day = hour * 24
+week = day * 7
+month = day * 30
+year = day * 256
